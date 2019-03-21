da5de6e4b63d1abffad4ad1a25988054d6fea306
Support <umic@microcontrol.net>
2019-03-21 14:11:02 +0100

    Initial commit

---
+++ qt_demo.cpp
@@ -0,0 +1,25 @@
+#include <QtCore/QDebug>
+#include <QtCore/QThread>
+#include <QtCore/QString>
+
+
+
+void hello(QString name)
+{
+	qDebug() << "Hello" << name << "from" << QThread::currentThread();
+}
+
+int main(int argc, char **argv)
+{
+   Q_UNUSED(argc);
+   Q_UNUSED(argv);
+
+   QString clVersionT;
+
+   printf("Compiled with Qt Version %s\n", QT_VERSION_STR);
+   hello("µMIC.200");
+
+
+}
+
+
